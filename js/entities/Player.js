5c7526e1ad77d10d6dab0697dd5dbd6992278993
Gerard van der Honing <g.vanderhoning@gmail.com>
2014-04-15 10:29:51 +0200

    added gridbased collision detection

--- js/entities/Player.js
+++ js/entities/Player.js
@@ -19,6 +19,7 @@ game.playerObject = me.ObjectEntity.extend({
 			
 			//tag as player
 			this.type = me.game.world.PLAYER;
+			//this.collisionMap = me.game.currentLevelgetLayerByName("Drawthisplease").getTile(this.pos.x, this.pos.y);
 		},
 
 		update : function () {
@@ -42,7 +43,12 @@ game.playerObject = me.ObjectEntity.extend({
 				this.vel.x += this.accel.x * me.timer.tick;
 				this.vel.y += this.accel.y * me.timer.tick;
 			}
-
+			
+			//console.log("fps:" +me.sys.fps);
+			//console.log("collsionMap: "+me.game.collisionMap.orientation);
+			console.log(me.game.currentLevel.getLayerByName("Drawthisplease"));
+			console.log("collsionMapTile: "+me.game.currentLevel.getLayerByName("Drawthisplease").getTileId(this.pos.x,this.pos.y));
+			console.log("collsionTileShape: "+me.game.currentLevel.getLayerByName("Drawthisplease").shapeType);
 			this.updateMovement();
 			this.parent();
 			return true;
