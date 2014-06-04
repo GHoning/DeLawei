7d08752ebb9a65c39615d4182ab3920ec5eed160
justinbotger <justinbotger@gmail.com>
2014-06-04 12:40:12 +0200

    instruction screen added
    Buttons don't work for some unexplanable reason.

--- js/screens/instructions.js
+++ js/screens/instructions.js
@@ -1,37 +1,56 @@
 game.InstructionsScreen = me.ScreenObject.extend({
-	onResetEvent: function() {
-		this.alwaysUpdate = true;
-		//me.game.world is the main container for the game. And the 'add' is replaced by addChild
-		me.game.world.addChild(new game.InstructionsScreen.menuButton(100, 100, {image: "menuButton", spritewidth: 256,spriteheight: 128}));
-	}
-});
+		onResetEvent : function () {
+			this.alwaysUpdate = true;
+			//me.game.world is the main container for the game. And the 'add' is replaced by addChild
+			me.game.world.addChild(new game.InstructionsScreen.MenuButton(50, 640, {
+					image : "menuButton",
+					spritewidth : 256,
+					spriteheight : 128
+				}));
+			me.game.world.addChild(new game.InstructionsScreen.PlayButton(650, 640, {
+					image : "playButton",
+					spritewidth : 256,
+					spriteheight : 128
+				}));
+			me.game.world.addChild(new game.UIImage(0, 0, "introductiescherm"));
+		},
 
-game.InstructionsScreen.menuButton = me.ObjectEntity.extend({
-	init : function (x, y, settings) {
-		this.parent(x, y, settings);
-		this.keyLock = false;
-		this.floating = true;
-		this.imgButton = new me.AnimationSheet(this.pos.x, this.pos.y, me.loader.getImage("menuButton"), 256, 128);
-		//add Shape in MelonJS 1.0.0 for the collision box
-		this.rect = new me.Rect(this.pos, 256, 128);
-		this.addShape(this.rect);
-	},
+	});
 
-	update : function () {
-			
-		if (this.getShape().containsPointV(me.input.mouse.pos) && me.input.isKeyPressed("mouse/touch") && !this.keyLock) {
-			this.keyLock = true;
+game.InstructionsScreen.MenuButton = game.UIButton.extend({
+
+		init : function (x, y, settings) {
+			this.parent(x, y, settings);
+			console.log("init");
+		},
+
+		onClick : function () {
+			console.log("onClick");
 			me.state.change(me.state.MENU);
-		}
+		},
 
-		if (!me.input.isKeyPressed("mouse/touch")) {
-			this.keyLock = false;
-		}
-			
-		return true;
-	},
+		onHover : function (bool) {
+			if (bool) {
+				console.log("hover");
+			}
+		},
+	});
 
-	draw : function (context) {
-		this.imgButton.draw(context);
-	}
-});
+game.InstructionsScreen.PlayButton = game.UIButton.extend({
+
+		init : function (x, y, settings) {
+			this.parent(x, y, settings);
+			console.log("init");
+		},
+
+		onClick : function () {
+			console.log("onClick");
+			me.state.change(me.state.PLAY);
+		},
+
+		onHover : function (bool) {
+			if (bool) {
+				console.log("hover");
+			}
+		},
+	});
--- js/screens/menu.js
+++ js/screens/menu.js
@@ -3,7 +3,6 @@ game.MenuScreen = me.ScreenObject.extend({
 		//me.game.world is the main container for the game. And the 'add' is replaced by addChild
 		me.game.world.addChild(new game.MenuScreen.playButton(100, 100, {image: "playButton", spriteWidth: 256,spriteHeight: 128}));
 		me.game.world.addChild(new game.MenuScreen.creditButton(100, 515, {image: "creditsButton", spriteWidth: 256,spriteHeight: 128}));
-		me.game.world.addChild(new game.MenuScreen.instructionsButton(515, 100, {image: "instructionsButton", spriteWidth: 256,spriteHeight: 128}));
 		me.game.world.addChild(new game.MenuScreen.nhllogo(0, 0, "nhllogo"));
 	}
 });
@@ -14,7 +13,7 @@ game.MenuScreen.playButton = game.UIButton.extend({
 	},
 
 	onClick : function () {
-		me.state.change(me.state.PLAY);
+		me.state.change(me.state.INSTRUCTIONS);
 	},
 	
 	onHover : function () {
@@ -28,23 +27,8 @@ game.MenuScreen.creditButton = game.UIButton.extend({
 	},
 
 	onClick : function () {
-		//TODO addStateChange
 		console.log("credits");
-	},
-	
-	onHover : function () {
-	
-	}
-});
-
-game.MenuScreen.instructionsButton = game.UIButton.extend({
-	init : function (x, y, settings) {
-		this.parent(x, y, settings);
-	},
-	
-	onClick : function () {
-		//TODO addStateChange
-		console.log("instructions");
+		me.state.change(me.state.CREDITS);
 	},
 	
 	onHover : function () {
